47e2a9233fe4cfa2784a2e0bbdbbc2e6e8764b97
Murad718 <66924369+Murad718@users.noreply.github.com>
2021-10-29 13:45:08 +0300

    Commit1

---
+++ components/Message.vue
@@ -0,0 +1,35 @@
+<template> 
+  <article class="message">
+  	<h2>{{message.title}}</h2>
+  	<p>{{message.body}}</p>
+    <form v-if="update">
+      <input v-model="message.title"  placeholder="Тема..." type="text">
+      <textarea v-model="message.body"  placeholder="Сообщение..." type="text"></textarea>
+      <button @click.prevent="updateMessage(message, message.id); update = false">Сохранить</button>
+    </form>
+  	<div class="message_delete message_button" @click="deleteMessage(message.id)"><i class="far fa-trash-alt"></i></div>
+    <div class="message_update message_button" @click="update = true"><i class="fas fa-pencil-alt"></i></div>
+  </article>
+</template>
+
+<script>
+
+import {mapGetters, mapActions} from 'vuex'	
+
+export default {
+	props:[
+		'message',
+	],
+  data(){
+    return{
+      update:false,
+    }
+  },
+	methods:{
+    ...mapActions({
+      deleteMessage:'message/deleteMessage',
+      updateMessage:'message/updateMessage',
+    }),
+  },
+}
+</script>
---
+++ store/message.js
@@ -0,0 +1,52 @@
+import { request } from '@/services/index.js'
+
+export const state = () => ({
+	messages:[],
+})
+
+export const mutations = {
+	setMessages(state, messages){
+		state.messages = messages;
+	},	
+}
+
+export const actions = {
+	async fetchMessages({commit}){	
+		try{
+			let messages = await request('message/','get');
+			commit('setMessages', messages)
+		}catch(err){
+			console.log(err)
+		}
+	}, 
+	async addMessage({commit,dispatch}, message){	
+		try{
+      let response = await request('message/','post', {...message});
+      dispatch('fetchMessages');
+    }catch (err) {
+      console.log(err)
+    }
+	}, 
+	async deleteMessage({commit,dispatch}, id){	
+		try{
+      let response = await request(`message/${id}`,'delete');
+      dispatch('fetchMessages');
+    }catch (err) {
+      console.log(err)
+    }
+	},
+	async updateMessage({commit,dispatch}, message, id){	
+		try{
+      let response = await request(`message/${id}`, 'put', {...message});
+      dispatch('fetchMessages');
+    }catch (err) {
+      console.log(err)
+    }
+	},
+}
+
+export const getters = {
+	messages(state){
+		return state.messages;
+	}
+}
